6310b51616d9fa444117f82e33767b3663c5ba8a
Sebastiana Augenstein <sebastiana.alessandra.helga.augenstein@hs-furtwangen.de>
2021-04-22 14:10:19 +0200

    Aufgabe 2

--- AbgabeP2.1/app.js
+++ AbgabeP2.1/app.js
@@ -1,7 +1,7 @@
 "use strict";
 //* Aufgabe 1 - Basics
 function a1() {
-    let x = "Alles"; //* Inisaliserung String x mit dem Wert Alles
+    let x = "Alles"; //* Initialisierung String x mit dem Wert Alles
     console.log(x); //* wie System.out.print -> x = Platzhalter
     func1(); //* Hier wird Funktion aufgerufen -> Klar wird hier ausgegeben
     console.log("Logo!"); //* Ausgabe
@@ -14,4 +14,13 @@ function func1() {
 //*  b)
 //*  c)
 //* Aufgabe 2 - Kontinuierliche Variablenmanipulation
+function a2() {
+    let i = 9; //* Ini number i mit dem Wert 9
+    do {
+        console.log(i); //* Ausgabe i 
+        i = i - 1; //* 
+    } while (i > 0); //* wenn i größer 0 ist 
+}
+a2();
+//* Konsolenausgabe: 
 //# sourceMappingURL=app.js.map
--- AbgabeP2.1/app.ts
+++ AbgabeP2.1/app.ts
@@ -1,6 +1,6 @@
 //* Aufgabe 1 - Basics
 function a1(): void { //* Deklaration einer Funktion -> Rückgabewert void
-    let x: string = "Alles"; //* Inisaliserung String x mit dem Wert Alles
+    let x: string = "Alles"; //* Initialisierung String x mit dem Wert Alles
     console.log(x); //* wie System.out.print -> x = Platzhalter
     func1(); //* Hier wird Funktion aufgerufen -> Klar wird hier ausgegeben
     console.log("Logo!"); //* Ausgabe
@@ -17,3 +17,15 @@ function func1(): void {
 //*  c)
 
 //* Aufgabe 2 - Kontinuierliche Variablenmanipulation
+function a2(): void {
+    let i: number = 9; //* Ini number i mit dem Wert 9
+
+    do {
+        console.log(i); //* Ausgabe i 
+        i = i - 1; //* 
+    } while( i > 0); //* wenn i größer 0 ist 
+}
+
+a2();
+
+//* Konsolenausgabe: 
