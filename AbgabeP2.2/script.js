01a218983a67797119ef364b7eb253e943d90d73
Sebastiana Augenstein <sebastiana.alessandra.helga.augenstein@hs-furtwangen.de>
2021-04-29 17:43:19 +0200

    Aufgabe 1 b)

--- AbgabeP2.2/script.js
+++ AbgabeP2.2/script.js
@@ -7,5 +7,26 @@ var P2_2;
         return Math.min.apply(Math, args); // apply = Array zu übergeben anstatt einzelne Parameter, Math.min kann nur einzelne Parameter bekommen
     }
     // console.log(min(5, 3, 1, 8)); // Test
+    // b)
+    function isEven(n) {
+        if (n % 2 == 0)
+            return;
+        if (n + 1)
+            return;
+        else
+            return;
+    }
+    // Aufgabe 2 - Arrays
+    // a)
+    // b)
+    // c)
+    // Aufgabe 3 - Endlich was visuelles!
+    // a)
+    // b)
+    // c)
+    // d)
+    // e)
+    // f)
+    // g)
 })(P2_2 || (P2_2 = {}));
 //# sourceMappingURL=script.js.map
--- AbgabeP2.2/script.ts
+++ AbgabeP2.2/script.ts
@@ -1,9 +1,41 @@
 namespace P2_2 {
     // Aufgabe 1 - Mehr "langweilige" Konsolenausgaben
     // a)
-    function min(...args: any[]): number { // args = Name arguments = Befehl. var = beliebiger Datentyp (z.B in // args = Name  arguments = Befehl. var = beliebiger Datentyp (z.B in Situation wo ich nicht weiß was ich zurück bekomme) 
+    function min (...args: any[]): number { // args = Name arguments = Befehl. var = beliebiger Datentyp (z.B in // args = Name  arguments = Befehl. var =      beliebiger Datentyp (z.B in Situation wo ich nicht weiß was ich zurück bekomme) 
         return Math.min.apply(Math, args); // apply = Array zu übergeben anstatt einzelne Parameter, Math.min kann nur einzelne Parameter bekommen
     }
    // console.log(min(5, 3, 1, 8)); // Test
+   // b)
+    function isEven ( n: number ): void {
+        if (n % 2 == 0)
+            return;
+        if (n + 1)
+            return;
+        else
+            return;
+        }
+    
+
+   // c)
+    interface Student{
+        nameStudent(): String;
+        martikelnummer(): number;
+        studiengang(): String;
+    }
+    // Aufgabe 2 - Arrays
+    // a)
+    // b)
+    // c)
+
+
+
+    // Aufgabe 3 - Endlich was visuelles!
+    // a)
+    // b)
+    // c)
+    // d)
+    // e)
+    // f)
+    // g)
 
 }
