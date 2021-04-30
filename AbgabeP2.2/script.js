a19b59daa0aa2b6910c74c7a92627d32f9f7c644
Sebastiana Augenstein <sebastiana.alessandra.helga.augenstein@hs-furtwangen.de>
2021-04-30 22:36:06 +0200

    1b)

--- AbgabeP2.2/script.js
+++ AbgabeP2.2/script.js
@@ -6,22 +6,44 @@ var P2_2;
     function min(...args) {
         return Math.min.apply(Math, args); // apply = Array zu übergeben anstatt einzelne Parameter, Math.min kann nur einzelne Parameter bekommen
     }
-    // console.log(min(5, 3, 1, 8)); // Test
+    //console.log(min(5, 3, 1, 8); // Test
     // b)
     function isEven(n) {
-        if (n % 2 == 0)
-            return;
-        if (n + 1)
-            return;
+        if (n == 0)
+            return true;
+        if (n == 1)
+            return false;
+        if (n < 0)
+            return isEven(-n);
         else
-            return;
+            return isEven(n - 2);
     }
+    let st1 = {
+        nameStudent: "Maja",
+        martikelnummer: 21372,
+        studiengang: "OMB"
+    };
+    let st2 = {
+        nameStudent: "Helena",
+        martikelnummer: 21373,
+        studiengang: "MIB"
+    };
+    let st3 = {
+        nameStudent: "Thomas",
+        martikelnummer: 21374,
+        studiengang: "MKB"
+    };
+    let stA;
+    const stA = { nameStudent: "", martikelnummer: , studiengang: "" };
+    console.log(stA);
     // Aufgabe 2 - Arrays
     // a)
     // b)
     // c)
     // Aufgabe 3 - Endlich was visuelles!
     // a)
+    function backwards() {
+    }
     // b)
     // c)
     // d)
--- AbgabeP2.2/script.ts
+++ AbgabeP2.2/script.ts
@@ -1,27 +1,55 @@
 namespace P2_2 {
     // Aufgabe 1 - Mehr "langweilige" Konsolenausgaben
     // a)
-    function min (...args: any[]): number { // args = Name arguments = Befehl. var = beliebiger Datentyp (z.B in // args = Name  arguments = Befehl. var =      beliebiger Datentyp (z.B in Situation wo ich nicht weiß was ich zurück bekomme) 
-        return Math.min.apply(Math, args); // apply = Array zu übergeben anstatt einzelne Parameter, Math.min kann nur einzelne Parameter bekommen
+    function min(...args: number[]): number { // args = Name arguments = Befehl.  
+    return Math.min.apply(Math, args); // apply = Array zu übergeben anstatt einzelne Parameter, Math.min kann nur einzelne Parameter bekommen
     }
-   // console.log(min(5, 3, 1, 8)); // Test
-   // b)
-    function isEven ( n: number ): void {
-        if (n % 2 == 0)
-            return;
-        if (n + 1)
-            return;
-        else
-            return;
-        }
-    
+    //console.log(min(5, 3, 1, 8); // Test
+    // b)
+    function isEven ( n: number ): boolean {
+    if (n == 0) 
+        return true;
+    if (n == 1)
+        return false;
+    if (n < 0)
+        return isEven (-n);
+    else
+        return isEven(n - 2);
+    }
+    //Test
+    //console.log(isEven(50)); true     Ergebnis = 0
+    //console.log(isEven(75)); false    Ergebnis = 1
+    //console.log(isEven(-1)); false    Ergebnis = -5
+    // Bsp: mit 5 : 5 ist nicht 0 u. 1, 5-2 = 3, 3 ist nicht 0 u. 1, 3-2 = 1 -> 1 ist nicht 0, aber 1 -> false
 
-   // c)
-    interface Student{
-        nameStudent(): String;
-        martikelnummer(): number;
-        studiengang(): String;
+    // c)
+    interface Student {
+        nameStudent: String;
+        martikelnummer: number;
+        studiengang: String;
     }
+    let st1: Student = { 
+        nameStudent: "Maja",
+        martikelnummer: 21372,
+        studiengang: "OMB"
+    };
+    let st2: Student = { 
+        nameStudent: "Helena",
+        martikelnummer: 21373,
+        studiengang: "MIB"
+    };
+    let st3: Student = { 
+        nameStudent: "Thomas",
+        martikelnummer: 21374,
+        studiengang: "MKB"
+    };
+
+    let stA: Array<Student>[];
+    
+    const stA = { nameStudent: "", martikelnummer: , studiengang: "" };
+
+    console.log(stA);    
+
     // Aufgabe 2 - Arrays
     // a)
     // b)
@@ -31,6 +59,9 @@ namespace P2_2 {
 
     // Aufgabe 3 - Endlich was visuelles!
     // a)
+    function backwards(){
+        
+    }
     // b)
     // c)
     // d)
