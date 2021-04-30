ee41be90b808a00554a3c616c5c1a472c3a8e67f
Sebastiana Augenstein <sebastiana.alessandra.helga.augenstein@hs-furtwangen.de>
2021-04-30 23:10:46 +0200

    Canvas

--- AbgabeP2.2/script.ts
+++ AbgabeP2.2/script.ts
@@ -52,16 +52,113 @@ namespace P2_2 {
 
     // Aufgabe 2 - Arrays
     // a)
+   // function backwards() {
+
+
+    //}
     // b)
+    //function join( arr: number[]: number) {
+
+    //}
+
     // c)
 
 
 
     // Aufgabe 3 - Endlich was visuelles!
     // a)
-    function backwards(){
-        
-    }
+
+    let canvas: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById("myFirstCanvas");
+    let context: CanvasRenderingContext2D = canvas.getContext("2d");
+
+    context.fillStyle = "blue";  //Himmel
+    context.fillRect(0, 0, 500, 400);
+    context.fillStyle = "green";   //Wiese
+    context.fillRect(0, 250, 500, 300);
+
+    context.beginPath();   //Baumstamm
+    context.lineWidth = 3;
+    context.fillStyle = "brown";
+    context.moveTo(370, 210);
+    context.lineTo(350, 300);
+    context.lineTo(410, 300);
+    context.lineTo(390, 210);
+    context.lineTo(370, 210);
+    context.stroke();
+    context.fill();
+
+    context.beginPath();    //Blätter
+    context.lineWidth = 1;
+    context.strokeStyle = "black";
+    context.fillStyle = "darkgreen";
+    context.arc(360, 210, 30, 0, Math.PI * 2);
+    context.fill();
+    context.stroke();
+
+    context.beginPath();    //Blätter
+    context.lineWidth = 1;
+    context.strokeStyle = "black";
+    context.fillStyle = "darkgreen";
+    context.arc(380, 180, 30, 0, Math.PI * 2);
+    context.fill();
+    context.stroke();
+
+    context.beginPath();    //Blätter
+    context.lineWidth = 1;
+    context.strokeStyle = "black";
+    context.fillStyle = "darkgreen";
+    context.arc(400, 210, 30, 0, Math.PI * 2);
+    context.fill();
+    context.stroke();
+
+    context.lineWidth = 10; //Haus
+    context.fillStyle = "black";
+    context.fillRect(75, 140, 150, 110);
+    context.fillStyle = "brown";
+    context.fillRect(130, 190, 40, 60);
+
+    context.beginPath();    //Dach
+    context.fillStyle = "black";
+    context.moveTo(50, 140);
+    context.lineTo(150, 60);
+    context.lineTo(250, 140);
+    context.closePath();
+    context.fill();
+    context.stroke();
+
+    context.beginPath();    //Wolke
+    context.fillStyle = "white";
+    context.arc(340, 60, 40, 0, Math.PI * 2);
+    context.fill();
+
+    context.beginPath();    //Wolke
+    context.fillStyle = "white";
+    context.arc(370, 35, 30, 0, Math.PI * 2);
+    context.fill();
+
+    context.beginPath();    //Wolke
+    context.fillStyle = "white";
+    context.arc(385, 60, 40, 0, Math.PI * 2);
+    context.fill();
+
+    context.beginPath();    //Wolke2
+    context.fillStyle = "lightgrey";
+    context.arc(35, 60, 20, 0, Math.PI * 2);
+    context.fill();
+
+    context.beginPath();    //Wolke2
+    context.fillStyle = "lightgrey";
+    context.arc(80, 44, 25, 0, Math.PI * 2);
+    context.fill();
+
+    context.beginPath();    //Wolke2
+    context.fillStyle = "lightgrey";
+    context.arc(55, 60, 25, 0, Math.PI * 2);
+    context.fill();
+
+    console.log(canvas);
+
+
     // b)
     // c)
     // d)
