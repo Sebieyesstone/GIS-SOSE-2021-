6e22738998ffac751e573237208424381bb06ec5
Sebastiana Augenstein <sebastiana.alessandra.helga.augenstein@hs-furtwangen.de>
2021-05-09 17:10:24 +0200

    Kommentare

--- AbgabeP2.3/Aufgabe2/script.js
+++ AbgabeP2.3/Aufgabe2/script.js
@@ -2,11 +2,11 @@
 // in Zusammenarbeit mit Anna-Lara, Benjamin und Tabea
 var Aufgabe02;
 (function (Aufgabe02) {
-    //const save: HTMLElement = document.getElementById("save");
+    //const save: HTMLElement = document.getElementById("save");           //wird später verwendet
     const anzeigeflaeche = document.getElementById("anzeigeflaeche");
-    class Person {
+    class Tier {
         setHead(_head) {
-            this.head = _head;
+            this.head = _head; // spätere Var.name setzen wir mit einer Id fest
         }
         setBody(_body) {
             this.body = _body;
@@ -18,29 +18,28 @@ var Aufgabe02;
             this.name = _name;
         }
     }
-    let person = new Person();
-    //nextHead.addEventListener("click", auswahlKopf); //"Create" als erster Schritt
+    let tier = new Tier(); // ini Tierobjekt
     for (let i = 0; i < kopfBilder.length; i++) {
-        const imgElem = document.createElement("img");
-        imgElem.src = kopfBilder[i];
-        imgElem.className = "auswahlbilder";
-        imgElem.id = String(i + 1);
-        anzeigeflaeche.appendChild(imgElem);
+        const imgElem = document.createElement("img"); //wir erstellen Bilder mit dem img-tag -> const = konstante 
+        imgElem.src = kopfBilder[i]; //für jedes bild erstellen wir ein img - v -> mit img tag kommt von unserem images Ordner (Dateipfad)
+        imgElem.className = "auswahlbilder"; //geben allen Bilder eine Klasse
+        imgElem.id = String(i + 1); //übergabe einer id (geht einmal alle bilder durch) damit man 
+        anzeigeflaeche.appendChild(imgElem); //geht auf die Anfl. zeigt neue Bilder an
     }
-    const optionsHead = document.querySelectorAll(".auswahlbilder");
+    const optionsHead = document.querySelectorAll(".auswahlbilder"); // neue V obH -> alle HTML El. die Klasse Auswahlbilder haben
     function highlightSelection(element) {
         optionsHead.forEach(element => {
-            element.style.border = "4px solid transparent";
-            element.style.boxShadow = "2px 3px 7px rgba(0, 0, 0, 0.3)";
+            element.style.border = "4px solid transparent"; // für jedes E. soll das ausgeführt werden
+            element.style.boxShadow = "2px 3px 7px rgb(188,143,143)";
         });
-        element.style.border = "4px solid #000";
-        element.style.boxShadow = "3px 4px 7px rgba(0, 0, 0, 0.7)";
+        element.style.border = "4px solid rgb(188,143,143)"; // Wenn man Bild anklickt -> Rahmenfarbe
+        element.style.boxShadow = "3px 4px 7px rgb(188,143,143)"; // leuchtet
     }
     optionsHead.forEach(element => {
         element.addEventListener("click", function () {
-            person.setHead(element.id);
-            highlightSelection(element);
-            console.log(person.head);
+            tier.setHead(element.id); // Person bekommt setHead Methode + id  (id des Kopfes wird übergeben)
+            highlightSelection(element); // Hervorhebung
+            console.log(tier.head); // Zahl des Bildes wird angezeigt (Prüfung)
         });
     });
 })(Aufgabe02 || (Aufgabe02 = {}));
--- AbgabeP2.3/Aufgabe2/script.ts
+++ AbgabeP2.3/Aufgabe2/script.ts
@@ -1,20 +1,20 @@
 // in Zusammenarbeit mit Anna-Lara, Benjamin und Tabea
 namespace Aufgabe02 {
     
-    //const save: HTMLElement = document.getElementById("save");
+    //const save: HTMLElement = document.getElementById("save");           //wird später verwendet
     const anzeigeflaeche: HTMLElement = document.getElementById("anzeigeflaeche");
-    class Person {
+    class Tier {
         head: string;
         body: string;
         legs: string;
         name: string;
 
-        setHead(_head: string): void {
-            this.head = _head;
+        setHead(_head: string): void {                                                        // Zahl wird gespeichert von dem Kopf den ich auswähle - Zwischenspeicher meiner ersten Auswahl
+            this.head = _head;                                                                // spätere Var.name setzen wir mit einer Id fest
         }
 
-        setBody(_body: string): void {
-            this.body = _body;
+        setBody(_body: string): void {                                                        // später für die nächsten Aufgaben relevant
+            this.body = _body; 
         }
 
         setLegs(_legs: string): void {
@@ -25,34 +25,33 @@ namespace Aufgabe02 {
             this.name = _name;
         }
     }
-    let person: Person = new Person();
+    let tier: Tier = new Tier();                                                               // ini Tierobjekt
 
-    //nextHead.addEventListener("click", auswahlKopf); //"Create" als erster Schritt
-    for (let i: number = 0; i < kopfBilder.length; i++) {
-        const imgElem: HTMLImageElement = document.createElement("img");
-        imgElem.src = kopfBilder[i];
-        imgElem.className = "auswahlbilder";
-        imgElem.id = String(i + 1);
-        anzeigeflaeche.appendChild(imgElem);
+    for (let i: number = 0; i < kopfBilder.length; i++) { 
+        const imgElem: HTMLImageElement = document.createElement("img");                       //wir erstellen Bilder mit dem img-tag -> const = konstante 
+        imgElem.src = kopfBilder[i];                                                           //für jedes bild erstellen wir ein img - v -> mit img tag kommt von unserem images Ordner (Dateipfad)
+        imgElem.className = "auswahlbilder";                                                   //geben allen Bilder eine Klasse
+        imgElem.id = String(i + 1);                                                            //übergabe einer id (geht einmal alle bilder durch) damit man 
+        anzeigeflaeche.appendChild(imgElem);                                                   //geht auf die Anfl. zeigt neue Bilder an
     }
 
-    const optionsHead: NodeListOf<HTMLElement> = document.querySelectorAll(".auswahlbilder");
+    const optionsHead: NodeListOf<HTMLElement> = document.querySelectorAll(".auswahlbilder");  // neue V obH -> alle HTML El. die Klasse Auswahlbilder haben
 
-    function highlightSelection(element: HTMLElement): void {
-        optionsHead.forEach(element => {
-            element.style.border = "4px solid transparent";
-            element.style.boxShadow = "2px 3px 7px rgba(0, 0, 0, 0.3)";
+    function highlightSelection(element: HTMLElement): void {                                  //Kopfbilder
+        optionsHead.forEach(element => {                                                       // => bedeutet Pfeil. Liste mit allen El. von dieser Klasse (alle BILDER die wir erstellt haben)
+            element.style.border = "4px solid transparent";                                    // für jedes E. soll das ausgeführt werden
+            element.style.boxShadow = "2px 3px 7px rgb(188,143,143)";                          
         });
 
-        element.style.border = "4px solid #000";
-        element.style.boxShadow = "3px 4px 7px rgba(0, 0, 0, 0.7)";
+        element.style.border = "4px solid rgb(188,143,143)";                                    // Wenn man Bild anklickt -> Rahmenfarbe
+        element.style.boxShadow = "3px 4px 7px rgb(188,143,143)";                               // leuchtet
     }
     
-    optionsHead.forEach(element => {
-        element.addEventListener("click", function (): void {
-            person.setHead(element.id);
-            highlightSelection(element);
-            console.log(person.head);
+    optionsHead.forEach(element => {                                                            // muss alles wieder ausgeführt werden
+        element.addEventListener("click", function (): void {                                   // beim Anklicken soll eine Funktion ausgeführt werden
+            tier.setHead(element.id);                                                           // Person bekommt setHead Methode + id  (id des Kopfes wird übergeben)
+            highlightSelection(element);                                                        // Hervorhebung
+            console.log(tier.head);                                                             // Zahl des Bildes wird angezeigt (Prüfung)
         });
     });
 }
