a642adc520cda269f86412f97b8f75fc375ae753
Sebastiana Augenstein <sebastiana.alessandra.helga.augenstein@hs-furtwangen.de>
2021-05-28 15:57:32 +0200

    Abgabe

--- AbgabeP3.1/index.js
+++ AbgabeP3.1/index.js
@@ -1,19 +1,27 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const url_1 = require("url");
 var Aufgabe3_1;
 (function (Aufgabe3_1) {
     let button = document.getElementById("button");
     button.addEventListener("click", handle);
     async function handle() {
         let formData = new FormData(document.forms[0]);
-        let url = "https://sebieyesstonegis2021.herokuapp.com";
-        let query = new URLSearchParams(formData);
-        url = url + "?" + query.toString();
-        await fetch(url);
+        let _url = "https://sebieyesstonegis2021.herokuapp.com";
+        let query = new url_1.URLSearchParams(formData);
+        _url = _url + "?" + query.toString();
+        await fetch(_url);
         for (let entry of query) {
             console.log(entry);
             console.log("name: " + entry[0]);
             console.log("value: " + entry[1]);
         }
+        let response = await fetch(_url);
+        let answer = await response.text();
+        console.log(answer);
+        let paragraph = document.createElement("p");
+        paragraph.innerText = answer;
+        document.body.appendChild(paragraph);
     }
 })(Aufgabe3_1 || (Aufgabe3_1 = {}));
 //# sourceMappingURL=index.js.map
--- AbgabeP3.1/index.ts
+++ AbgabeP3.1/index.ts
@@ -1,3 +1,5 @@
+import { URLSearchParams } from "url";
+
 namespace Aufgabe3_1 {
     let button: HTMLButtonElement = <HTMLButtonElement>document.getElementById("button");
     button.addEventListener("click", handle);
@@ -5,10 +7,10 @@ namespace Aufgabe3_1 {
     async function handle(): Promise<void> {
 
         let formData: FormData = new FormData(document.forms [0]);
-        let url: string = "https://sebieyesstonegis2021.herokuapp.com"; 
+        let _url: string = "https://sebieyesstonegis2021.herokuapp.com"; 
         let query: URLSearchParams = new URLSearchParams (<any>formData);
-        url = url + "?" + query.toString();
-        await fetch(url);
+        _url = _url + "?" + query.toString();
+        await fetch(_url);
 
         for (let entry of query) {
             console.log(entry);
@@ -16,5 +18,13 @@ namespace Aufgabe3_1 {
             console.log("value: " + entry[1]);
             
         }
+
+        let response: Response = await fetch(_url);
+        let answer: string = await response.text();
+        console.log(answer);
+
+        let paragraph: HTMLParagraphElement = document.createElement("p");
+        paragraph.innerText = answer;
+        document.body.appendChild(paragraph);
     }
 }
