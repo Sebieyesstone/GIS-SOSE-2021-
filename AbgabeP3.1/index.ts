57505802a23946bf7c1c1f7a78f9291e4891d160
Sebastiana Augenstein <sebastiana.alessandra.helga.augenstein@hs-furtwangen.de>
2021-06-02 14:03:32 +0200

    Verbesserung

--- AbgabeP3.1/index.ts
+++ AbgabeP3.1/index.ts
@@ -5,11 +5,11 @@ namespace Aufgabe3_1 {
 
     async function handle(): Promise<void> {
         let formData: FormData = new FormData(document.forms[0]);
-        let _url: RequestInfo = "https://sebieyesstonegis2021.herokuapp.com"; 
+        let url: RequestInfo = "https://sebieyesstonegis2021.herokuapp.com"; 
         let query: URLSearchParams = new URLSearchParams (<any>formData);
-        _url = _url + "?" + query.toString();
-        console.log(_url);
-        await fetch(_url);
+        url = url + "?" + query.toString();
+        console.log(url);
+        await fetch(url);
 
         for (let entry of query) {
             console.log(entry);
@@ -17,7 +17,7 @@ namespace Aufgabe3_1 {
             console.log("value: " + entry[1]);
             
         }
-        let response: Response = await fetch(_url);
+        let response: Response = await fetch(url);
         let answer: string = await response.text();
         console.log(answer);
         let paragraph: HTMLParagraphElement = document.createElement("p");
