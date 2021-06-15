3347494ce8e416b993125ad624ffd3089697bbd3
Sebastiana Augenstein <sebastiana.alessandra.helga.augenstein@hs-furtwangen.de>
2021-06-15 11:31:17 +0200

    server

--- AbgabeP3.4/server.ts
+++ AbgabeP3.4/server.ts
@@ -4,50 +4,47 @@ import * as Mongo from "mongodb";
 
 export namespace Aufgabe3_4 {
 
-    let collection: Mongo.Collection;
-    let databaseUrl: string = "mongodb+srv://Testuser:<GIS404>@sebieyesstonegis-ist-ge.oawwp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-    connectToDatabase(databaseUrl);
+    let _url: string = "mongodb+srv://Testuser:GIS404@sebieyesstonegis-ist-ge.oawwp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-    console.log("Starting Server");
-
-    let port: number = Number(process.env.PORT);
+    let mongoCollection: Mongo.Collection;
+    let port: number | string | undefined = Number(process.env.PORT);
 
     if (!port)
         port = 8100;  //Port wird auf 8100 gesetzt
+    
+    startServer(port);
+    connectDatabase();
 
-    let server: Http.Server = Http.createServer(); //Server wird erstellt
-
-    server.addListener("request", handleRequest);  //Funktion handleRequest wird aufgerufen
-
-    server.addListener("listening", handleListen); //HandleListen Funktion wird aufgerufen
-
-    server.listen(port);
-
-    async function connectToDatabase(url: string): Promise<void> {
-
-        let options: Mongo.MongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true};
-        let mongoClient: Mongo.MongoClient = new Mongo.MongoClient(url, options);
-        await mongoClient.connect();
-        
-        collection = mongoClient.db("Test").collection("Students");
+    function startServer(_port: number | string): void {
+        let server: Http.Server = Http.createServer();
+        server.addListener("request", handleRequest);
+        server.listen(port);
     }
-    function handleListen(): void {
-        console.log("Listening");
+    async function connectDatabase(): Promise<void> {
+        let options: Mongo.MongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true};
+        let mongoClient: Mongo.MongoClient = new Mongo.MongoClient(_url, options);
+        await mongoClient.connect();
+        mongoCollection = mongoClient.db("Test").collection("Students");
     }
     async function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): Promise<void> {
+
         _response.setHeader("content-type", "text/html; charset=utf-8"); 
         _response.setHeader("Access-Control-Allow-Origin", "*"); 
 
         if (_request.url) {
             let url: Url.UrlWithParsedQuery = Url.parse(_request.url, true);
-            let pathname: string | null = url.pathname;
       
-            if (pathname == "/abschicken") {
-      
-              collection.insertOne(url.query);
-      
-            } else if (pathname == "/erhalten") {
-              _response.write(JSON.stringify(await collection.find().toArray()));
+            if (url.pathname == "/insert") {
+                mongoCollection.insert(url.query);
+        
+            } 
+            else if (url.pathname == "/pull") {
+              let findings: Mongo.Cursor<string> = mongoCollection.find();
+              let findingsArray: string [] = await findings.toArray();
+              _response.write(JSON.stringify(findingsArray));
+            }
+            else {
+                console.log("Error - Daten nicht vorhanden");
             }
         }
       
