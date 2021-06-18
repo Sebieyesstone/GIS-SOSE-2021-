c21364cd7d3155c67bd9c4abb050d9b494eb01d3
Sebastiana Augenstein <sebastiana.alessandra.helga.augenstein@hs-furtwangen.de>
2021-06-18 19:22:58 +0200

    Update server.ts

--- AbgabeP3.4/server.ts
+++ AbgabeP3.4/server.ts
@@ -12,8 +12,7 @@ export namespace Aufgabe3_4 {
     }*/
 
     let mongoCollection: Mongo.Collection;
-    let mongoUrl: string = "mongodb+srv://Testuser:GIS404@sebieyesstonegis-ist-ge.oawwp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-    //let mongoUrl: string = "mongodb+srv://Testuser:GIS404@sebieyesstonegis-ist-ge.oawwp.mongodb.net";
+    let mongoUrl: string = "mongodb+srv://Testuser:GIS404@sebieyesstonegis-ist-ge.oawwp.mongodb.net";
     //let mongoUrl: string = "mongodb://localhost:27017";
 
     let port: number = Number(process.env.PORT);
